cc4328e8a81a1017b899390382d91ecdb7b08da2
Abdullah Azam Khan <ranaabdullahazam102@gamil.com>
2025-01-22 18:41:59 +0500

    add role variable in login function or add cors

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-
-
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const connectDB = require('./src/config/database'); 
-
 const routes = require('./src/routes/index');
-const app = express()
 
-// mongoose conntetion
+const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: 'http://localhost:5173', // Your Vite frontend URL
+  credentials: true // Allow cookies and credentials
+}));
+
+// mongoose connection
 connectDB();
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use('/api', routes)
-
+app.use('/api', routes);
 
 // Start the Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
