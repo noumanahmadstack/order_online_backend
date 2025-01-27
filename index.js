59f98287ff786ef5568221dc0092509b2ea3639f
Abdullah Azam Khan <ranaabdullahazam102@gamil.com>
2025-01-27 23:01:48 +0500

    image resize

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+// server.js
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const connectDB = require('./src/config/database'); 
+const connectDB = require('./src/config/database');
 const routes = require('./src/routes/index');
 
 const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', // Your Vite frontend URL
-  credentials: true // Allow cookies and credentials
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
+
+// Increase payload size limit for JSON and URL-encoded data
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // mongoose connection
 connectDB();
 
-// Middleware to parse JSON requests
-app.use(express.json());
 app.use('/api', routes);
 
-// Start the Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
