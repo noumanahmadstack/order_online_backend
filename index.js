51928e7ce71b7f8001c1948badf66700b122c8b3
noumanahmadstack <noumanahmad056@gmail.com>
2025-02-27 18:01:02 +0500

    code fixes

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get("",(req,res)=>{
+  return res.status(200).send('server is running successfully on port')
+}
+)
